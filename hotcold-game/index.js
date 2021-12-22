b88c0622f8f4dd2761d02368563852f233423b24
Val Laf <valerie.lafaiteur@gmail.com>
2021-12-22 08:12:42 +0100

    button commencer partie

--- hotcold-game/index.js
+++ hotcold-game/index.js
@@ -1,12 +1,21 @@
-const mysteryNumber = Math.floor(Math.random() *100) +1;
-console.log(mysteryNumber);
 const beginGame = document.querySelector(".begin-game-btn");
+const choiceValidation = document.querySelector("#button-validation");
+let numberChosenInput =document.getElementById('choose-number').value;
 const displayGameContainer = document.querySelector(".gameplay-container");
 
 beginGame.addEventListener("click",() => {
-beginGame.classList.add("begin-game-btn-clicked");
-displayGameContainer.style.visibility = "visible";
+    beginGame.classList.add("begin-game-btn-clicked");
+    displayGameContainer.style.visibility = "visible";
+    const mysteryNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(mysteryNumber);
 })
+
+choiceValidation.addEventListener("click", () => {
+
+   console.log(numberChosenInput);
+})
+
+
 
 
 
