11bf37df524155a9a8717d8da2828d870f8d5286
Val Laf <valerie.lafaiteur@gmail.com>
2021-12-23 20:48:36 +0100

    html add div id=" message"

--- hotcold-game/index.js
+++ hotcold-game/index.js
@@ -1,14 +1,19 @@
+//  commencer la partie : faire apparaitre le jeu
 const displayGameContainer = document.querySelector(".gameplay-container");
 const displayRulesContainer = document.querySelector(".rules-container");
 const beginGame = document.querySelector(".begin-game-btn");
 
-const choiceValidation = document.getElementById('value-submit');
-let numberInput = document.getElementById('number-input');
-let resultText =  document.getElementById('result');
+// déclarer le nombre à trouver
 const mysteryNumber = Math.floor(Math.random() * 100) + 1;
 console.log(mysteryNumber);
+
+//
+let validButton = document.getElementById('submit');
+let numberInput = document.getElementById('number-input');
+let resultsDiv =  document.getElementById('result');
+let message = document.querySelector('#message');
 let numberInputList = [];
-const trialNumber = 10;
+const trialsNumber = 10;
 
 beginGame.addEventListener("click", () => {
     beginGame.classList.add("begin-game-btn-clicked");
@@ -16,22 +21,36 @@ beginGame.addEventListener("click", () => {
     displayRulesContainer.style.visibility = "hidden";
 })
 
-function compare() {
-    for (let i = numberInputList.length; i < trialNumber; i++) {
-        if (numberInput < mysteryNumber) {
-            resultText.textContent = 'Choisis un nombre supérieur';
-              } else if (numberInput > mysteryNumber) {
-            resultText.textContent = 'Choisis un nombre inférieur';
-        } else if (numberInput === mysteryNumber) {
-            document.querySelector('#result').textContent = 'Gagné !!!';
+validButton.addEventListener('click', () => {
+    if (numberInput.value.trim()) {
+        numberInputList.push(parseInt(numberInput.value));
+        displayResults();
+
+        if(numberInputList.length === trialsNumber) {
+            message.innerText = "GAME OVER"
+        } else {
+            let inputValue = parseInt(numberInput.value);
+            console.log(inputValue);
+            if(inputValue < mysteryNumber) {
+                message.innerText = "Choisissez un chiffre plus grand";
+            } else if( inputValue > mysteryNumber) {
+                message.innerText = "Choisissez un chiffre plus petit";
+            } else {
+                message.innerText = "Vous avez gagné ";
+            }
         }
     }
-}
-choiceValidation.addEventListener("click", () => {
-    numberInputList.push(parseInt(numberInput.value));
-    document.getElementById("number-input").value = "";
-    console.log(numberInputList);
-    compare();
+    numberInput.value = "";
+});
 
-})
+function displayResults() {
+    resultsDiv.innerHTML ="";
+    numberInputList.forEach((result)=> {
+        let div = document.createElement('div');
+        div.innerHTML = "<h3>Nombre choisi : </h3>"+ result;
+        resultsDiv.appendChild(div);
+    });
+}
+
+
 
